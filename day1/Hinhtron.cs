06213f6e76b85ab23bceb41a30d09c8a09557aca
Thaihoang <Thaihoang.nth06@gmail.com>
2026-09-18 21:15:01 +0700

    hinhtron

---
+++ day1/Hinhtron.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ExampleCAdvance.Hinh
+{
+    public class HinhTron
+    {
+        private double banKinh;
+
+        public double BanKinh
+        {
+            get { return banKinh; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Ban kinh phai lon hon 0");
+                }
+
+                banKinh = value;
+            }
+        }
+
+        public HinhTron()
+        {
+        }
+
+        public HinhTron(double banKinh)
+        {
+            BanKinh = banKinh;
+        }
+
+        public double TinhChuVi()
+        {
+            return 2 * Math.PI * BanKinh;
+        }
+
+        public double TinhDienTich()
+        {
+            return Math.PI * BanKinh * BanKinh;
+        }
+
+        public void HienThi()
+        {
+            Console.WriteLine($"Ban kinh: {BanKinh}");
+            Console.WriteLine($"Chu vi: {TinhChuVi():F2}");
+            Console.WriteLine($"Dien tich: {TinhDienTich():F2}");
+        }
+    }
+}
